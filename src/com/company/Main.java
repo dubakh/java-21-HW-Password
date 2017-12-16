98590b898c01adfdfebc1aa5f97c6f0d3952cc14
Dubakh <dubakh@gmail.com>
2017-12-16 16:44:06 +0200

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,75 @@
+package com.company;
+
+import java.io.*;
+
+public class Main {
+
+    public static boolean success = false;
+
+    public static void main(String[] args) throws IOException {
+
+        String attemptStorage = "/Users/boris/Desktop/file.txt";
+        String passwordStorage = "/Users/boris/Desktop/Password.txt";
+
+        System.out.println("Please, enter the password:");
+
+        while (!success) {
+          BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+          String attemptFromConsole = bufferedReader.readLine();
+          addTofile(attemptFromConsole, attemptStorage);
+          compareAttemptWithPassword(attemptStorage, passwordStorage);
+        }
+
+    }
+
+    private static boolean compareAttemptWithPassword(String attemptStorage, String passwordStorage) throws IOException {
+        InputStream temp = new FileInputStream(attemptStorage);
+        InputStream password = new FileInputStream(passwordStorage); //Main.class.getResourceAsStream("/password.txt");
+        try {
+            while (true) {
+                int fr = temp.read();
+                int tr = password.read();
+
+                if (fr != tr) {
+                    System.out.println("Wrong password");
+                    return false;
+                }
+
+                if (fr == -1) {
+                    System.out.println("Right password");
+                    success = true;
+                    return true;
+                }
+            }
+
+        } finally {
+            if (temp != null)
+                temp.close();
+            if (password != null)
+                password.close();
+        }
+    }
+
+    private static void addTofile(String attemptFromConsole, String attemptStorage) throws IOException {
+        BufferedWriter bufferedWriter = null;
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(attemptStorage, false);
+            bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(attemptFromConsole);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+            if (fileWriter != null) {
+                fileWriter.close();
+            }
+        }
+    }
+}
+
+
+
+
